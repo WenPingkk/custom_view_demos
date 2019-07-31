c2ae8a42df5c0f986ee0e8eca6d715b63111f38d
wenpingkk <wenpingkk@163.com>
2019-07-31 22:06:48 +0800

    18 dialog_02

--- 08_custom_dialog/src/main/java/com/sean/dialog/view/CustomDialog.java
+++ 08_custom_dialog/src/main/java/com/sean/dialog/view/CustomDialog.java
@@ -7,8 +7,15 @@ import android.view.Gravity;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
+import com.bigkoo.pickerview.adapter.ArrayWheelAdapter;
+import com.contrarywind.listener.OnItemSelectedListener;
+import com.contrarywind.view.WheelView;
 import com.sean.dialog.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author WenPing
@@ -19,6 +26,10 @@ public class CustomDialog extends Dialog {
 
 
     private Window mWindow;
+    private WheelView mHourWheelView;
+    private WheelView mMinWheelView;
+    private TextView mTvHour;
+    private TextView mTvMin;
 
     public CustomDialog(Context context) {
         this(context, 0);
@@ -53,6 +64,50 @@ public class CustomDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_dialog);
+        initData();
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        mHourWheelView.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int index) {
+                mTvHour.setText(hourDatas.get(index));
+            }
+        });
+        mMinWheelView.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int index) {
+                mTvMin.setText(minData.get(index));
+            }
+        });
+    }
+
+    private void initData() {
+        for (int i = 0; i < 24; i++) {
+            hourDatas.add("" + i);
+        }
+
+        for (int i = 0; i < 60; i++) {
+            minData.add("" + i);
+        }
+    }
+
+    private List<String> hourDatas = new ArrayList<>();
+    private List<String> minData = new ArrayList<>();
+    private void initView() {
+        mTvHour = findViewById(R.id.tv_hour);
+        mTvMin = findViewById(R.id.tv_min);
+        mHourWheelView = findViewById(R.id.wheel_view_hour);
+        mMinWheelView = findViewById(R.id.wheel_view_min);
+
+        mHourWheelView.setAdapter(new ArrayWheelAdapter(hourDatas));
+        mMinWheelView.setAdapter(new ArrayWheelAdapter(minData));
+        mHourWheelView.setCurrentItem(0);
+        mMinWheelView.setCurrentItem(0);
+        mTvHour.setText(hourDatas.get(0));
+        mTvMin.setText(minData.get(0));
     }
 
     @Override
